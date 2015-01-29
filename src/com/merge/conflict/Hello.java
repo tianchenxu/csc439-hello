3e662d1c27096fa9db2cd29849362abd09c925a1
Rasheed <secr4sh@gmail.com>
2015-01-29 14:24:03 -0500

    changed Hello.java

--- src/com/merge/conflict/Hello.java
+++ src/com/merge/conflict/Hello.java
@@ -3,11 +3,11 @@ package com.merge.conflict;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Team member 1: a");
+		System.out.println("Team member 1 is a jerk!");
 		System.out.println("Team member 1: b");
-		System.out.println("Team member 2: 1");
+		System.out.println("Team member 2 is the best!");
 		System.out.println("Team member 2: 2");
-		System.out.println("Team member changes this line entirely");
+		System.out.println("I am serious, they are terrible!");
 	}
 	
 }
